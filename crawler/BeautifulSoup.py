031f4f8842d65ed415007dc03e62c69bbe53911a
15120572037 <3179117263@qq.com>
2017-08-17 20:06:12 +0800

    Add files via upload

---
+++ crawler/BeautifulSoup.py
@@ -0,0 +1,12 @@
+#coding=utf-8
+#!/usr/bin/env python
+
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("http://python123.io/ws/demo.html")
+#print r.text
+
+demo = r.text
+soup = BeautifulSoup(demo,"html.parser")
+print soup.prettify()
